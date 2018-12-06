cb2fbcf4fc7f46f44edb17e334af1f8dd8f820fd
tm-n443 <plmnbhuiokj@gmail.com>
2018-12-06 14:52:09 +0900

    test:GitPushの確認 １回目

--- app/src/main/java/com/example/g015c1153/aid/MemberAdd.kt
+++ app/src/main/java/com/example/g015c1153/aid/MemberAdd.kt
@@ -2,11 +2,42 @@ package com.example.g015c1153.aid
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.squareup.moshi.KotlinJsonAdapterFactory
+import com.squareup.moshi.Moshi
+import kotlinx.android.synthetic.main.activity_member_add.*
 
 class MemberAdd : AppCompatActivity() {
+
+    private val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()!!
+    private val memberAddAdapter = moshi.adapter(User::class.java)!!
+    var user = User()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_member_add)
+
+        memberMailSearch.setOnClickListener {
+
+            user.mailAddress = memberMailSearch.text.toString()
+
+            var json = memberAddAdapter.toJson(user)
+            val url = ValueResponse().serverIp + ""
+
+            json = CallOkHttp().postRun(url, json)
+            val fromJson = memberAddAdapter.fromJson(json)
+            if(fromJson != null){
+                user = fromJson
+                textUserName.text = user.firstName + user.secondName
+            }else{
+                memberMailSearch.error = ValueResponse().errorNoUser
+            }
+        }
+
+        memberAddButton.setOnClickListener {
+            val mMemberMailSearch = memberMailSearch.text.toString()
+            val mMemberNo = memberNo.text.toString()
+            val mMemberPosition = memberPosition.text.toString()
+        }
+
     }
 }
--- app/src/main/java/com/example/g015c1153/aid/ValueResponse.kt
+++ app/src/main/java/com/example/g015c1153/aid/ValueResponse.kt
@@ -5,6 +5,7 @@ class ValueResponse {
     val errorNoValue = "未入力です"
     val errorNumber = "数値が未入力です"
     val errorPasswordMismatch = "パスワードが不一致です"
+    val errorNoUser = "ユーザーが見つかりませんでした"
     val complete = "コンプリート"
     val errorMismatch = "入力が不正です"
     val serverIp = "http://192.168.8.107"
