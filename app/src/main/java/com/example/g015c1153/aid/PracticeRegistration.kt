528d430d5f34b6906aab263e3acca85311cc2f4f
yuichitakazawa <appleboy0606@gmail.com>
2019-01-17 16:41:26 +0900

    練習と試合の日程登録画面（layoutとActivity）

---
+++ app/src/main/java/com/example/g015c1153/aid/PracticeRegistration.kt
@@ -0,0 +1,12 @@
+package com.example.g015c1153.aid
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class PracticeRegistration : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_practice_registration)
+    }
+}
