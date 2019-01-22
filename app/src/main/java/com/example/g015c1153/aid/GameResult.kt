9b9f44bba76467eb0cb74f6b723cf6d865fad48a
yuichitakazawa <appleboy0606@gmail.com>
2019-01-22 16:40:46 +0900

    試合データ登録画面のlayoutの修正と試合結果表示画面のActivityとlayoutの作成

---
+++ app/src/main/java/com/example/g015c1153/aid/GameResult.kt
@@ -0,0 +1,12 @@
+package com.example.g015c1153.aid
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class GameResult : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_result)
+    }
+}
