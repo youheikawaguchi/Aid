60c1c385a228c3cd1a3bb7f7e718d99c6874ae12
newiydr <44495903+newiydr@users.noreply.github.com>
2019-01-28 13:45:07 +0900

    Add files via upload

---
+++ Splash.kt
@@ -0,0 +1,39 @@
+package com.example.g015c1153.aid
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.support.v7.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_splash.*
+
+
+class Splash : AppCompatActivity() {
+
+    private val mHandler = Handler()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        mHandler.postDelayed(mSplashTask, 1000)
+
+        val gifMovie: Int = R.drawable.lode
+        //val orugaMovi: Int = R.drawable.oruga
+
+        // gif画像のセット
+        Glide.with(this).load(gifMovie).into(gifView)
+        //Glide.with(this).load(orugaMovi).into(orugaView)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mHandler.removeCallbacks(mSplashTask)
+    }
+
+    private val mSplashTask = Runnable {
+        val intent = Intent(this, TopActivity::class.java)//画面遷移のためのIntentを準備
+        startActivity(intent)//実際の画面遷移を開始
+        finish()//現在のActivityを削除
+    }
+}
